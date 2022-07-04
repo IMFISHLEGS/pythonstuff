89f1043fe7396f9b7d1f9ba81e83da3a0cde2b2a
FISH LEGS <onitirishola@gmail.com>
2022-07-04 12:01:19 -0500

    initial commit

---
+++ hello1.py
@@ -0,0 +1,11 @@
+print ("hello world!")
+
+full_name = "goodness man"
+
+print (full_name)
+full_name = "black widdow"
+print (full_name)
+full_name = " the kkatanjin brothers"
+print (full_name)
+surname = "quadrishh"
+print (surname)
