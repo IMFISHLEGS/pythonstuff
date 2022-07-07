d625480756db15a8a898bba24328e9671174c10e
FISH LEGS <onitirishola@gmail.com>
2022-07-07 05:30:38 -0500

    added alot of new things

--- hello1.py
+++ hello1.py
@@ -17,6 +17,21 @@ money = "even if i cant see shit"
 print (money)
 dupe = "dupe has small bum bum"
 print (dupe)
+greetings = 'my boss yelled "go fuck yourself"'
+print (greetings)
+name = "raymond"
+greetings = "my name is not " + name
+print(greetings.split(" "))
+num = "66"
+print((int(num)) * 3)
 
 
 
+
+
+
+
+
+
+
+ 
