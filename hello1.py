61a257ec18b4e0ac85ea8fbc6aeba4b43702c35e
FISH LEGS <onitirishola@gmail.com>
2022-07-20 18:51:52 -0500

    added inuit feild

--- hello1.py
+++ hello1.py
@@ -1,11 +1,12 @@
-#making buttons functional and adding colours
-
+#creating inpuit feild
 
 from tkinter import *
 root = Tk()
+e = Entry(root)
+e.pack()
 def myClick():
-	myLabel = Label(root, text="hey! go fuck yourself!")
+	myLabel = Label(root, text="Hello " + e.get())
 	myLabel.pack()
-myButton = Button(root, text="click me", command=myClick, fg="blue", bg="black")
+myButton = Button(root, text="enter your name", command=myClick, fg="blue", bg="black")
 myButton.pack()
 root.mainloop()
